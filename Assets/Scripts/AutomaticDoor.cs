87af1ae9b82f16198081016c9cda6dff5f780bf3
Nick McCurdy <nick@nickmccurdy.com>
2020-04-18 11:09:37 -0400

    Make airlock doors open and close

--- Assets/Scripts/AutomaticDoor.cs
+++ Assets/Scripts/AutomaticDoor.cs
@@ -2,8 +2,44 @@
 
 public class AutomaticDoor : MonoBehaviour
 {
+    private GameObject door;
+    private MeshRenderer meshRenderer;
+    private bool open;
+    private float distanceLifted;
+
+    void Awake()
+    {
+        door = transform.Find("Door").gameObject;
+        meshRenderer = door.GetComponent<MeshRenderer>();
+    }
+
     void OnTriggerEnter()
     {
-        Destroy(transform.Find("Door").gameObject);
+        open = true;
+    }
+
+    void OnTriggerExit()
+    {
+        open = false;
+    }
+
+    void Update()
+    {
+        if (open)
+        {
+            if (distanceLifted < meshRenderer.bounds.size.y)
+            {
+                door.transform.Translate(Vector3.up * Time.deltaTime * 1.5f);
+                distanceLifted += Time.deltaTime * 1.5f;
+            }
+        } 
+        else
+        {
+            if (distanceLifted > 0)
+            {
+                door.transform.Translate(Vector3.down * Time.deltaTime * 1.5f);
+                distanceLifted -= Time.deltaTime * 1.5f;
+            }
+        }
     }
 }
