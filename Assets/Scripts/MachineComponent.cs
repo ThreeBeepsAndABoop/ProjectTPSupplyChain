c43ffd65cae18fba21e56a30a9b4377c7609da5c
James Kelly <dxk2294@rit.edu>
2020-04-17 22:50:13 -0700

    Faffing

---
+++ Assets/Scripts/MachineComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MachineComponentType {
+    Battery,
+    Coolant,
+    Motor,
+    Computer,
+    Compressor,
+}
+
+public class MachineComponent : MonoBehaviour
+{
+    [Range(0, 100)]
+    public double Condition = 100;
+    public MachineComponentType Type;
+
+    public GameObject DebugLabelPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = Instantiate(DebugLabelPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCondition();
+    }
+
+    void UpdateCondition() {
+        Condition -= BaseDeteriorationRate() * Time.deltaTime;
+    }
+
+    // measured in condition loss per second
+    double BaseDeteriorationRate() {
+        switch (Type) {
+            default:
+                return 1.0 / 60.0 * 100.0;
+        }
+    }
+}
