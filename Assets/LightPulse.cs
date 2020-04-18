bafd68f31264fedfacf7de2133c75d72ae7abd46
James Kelly <dxk2294@rit.edu>
2020-04-18 01:54:54 -0700

    Animate engine

---
+++ Assets/LightPulse.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class LightPulse : MonoBehaviour
+{
+    Light light;
+    float startingIntensity;
+    float time;
+
+    public float duration = 1.0f;
+    public AnimationCurve translationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        startingIntensity = light.intensity;
+        pulseIncreasing = true;
+    }
+
+    bool pulseIncreasing;
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        float max = translationCurve.Evaluate(duration);
+        float offset = pulseIncreasing ? translationCurve.Evaluate(time / duration) : (max - translationCurve.Evaluate(time / duration));
+        light.intensity = startingIntensity + offset;
+
+        if (time > duration)
+        {
+            pulseIncreasing = !pulseIncreasing;
+            time = 0;
+        }
+    }
+}
---
+++ Assets/LinearTranslation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearTranslation : MonoBehaviour
+{
+    public Vector3 translationOffset = Vector3.forward * .1f;
+    public float translationDuration = 1;
+    public AnimationCurve translationCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator AnimateForwardTranslation()
+    {
+        float t = 0.0f;
+        Vector3 startingPos = transform.localPosition;
+        animating = true;
+        movingForward = true;
+        //GameManager.Instance.RequestPlayButtonClickSound();
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * (Time.timeScale / translationDuration);
+
+            transform.localPosition = Vector3.Lerp(startingPos, startingPos + translationOffset, translationCurve.Evaluate(t)); ;
+            yield return 0;
+        }
+
+        movingForward = false;
+        Vector3 newPos = transform.localPosition;
+        t = 0.0f;
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * (Time.timeScale / translationDuration);
+
+            transform.localPosition = Vector3.Lerp(newPos, startingPos, translationCurve.Evaluate(t)); ;
+            yield return 0;
+        }
+        animating = false;
+    }
+
+    private bool animating;
+    private bool movingForward;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!animating)
+        {
+            StartCoroutine(AnimateForwardTranslation());
+        }
+    }
+}
