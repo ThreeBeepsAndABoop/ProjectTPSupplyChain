7decc5cef70b9a30bf378d5abc535b9a2493dcbe
James Kelly <dxk2294@rit.edu>
2020-04-17 23:15:59 -0700

    Still faffing about with basic C# nonsense

--- Assets/Scripts/MachineComponent.cs
+++ Assets/Scripts/MachineComponent.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 public enum MachineComponentType {
     Battery,
@@ -12,20 +14,22 @@ public enum MachineComponentType {
 
 public class MachineComponent : MonoBehaviour
 {
-    [Range(0, 100)]
-    public double Condition = 100;
+    [Range(0, 1)]
+    public double Condition = 1;
     public MachineComponentType Type;
 
     public GameObject DebugLabelPrefab;
+
+    private TextMeshPro _debugText;
 
     // Start is called before the first frame update
     void Start()
     {
         GameObject go = Instantiate(DebugLabelPrefab);
-        go.transform.position = transform.position + new Vector3(0.0f, 0.25f, 0.0f);
+        go.transform.position = transform.position + new Vector3(0.0f, 0.3f, 0.0f);
         go.transform.parent = transform;
-
         go.transform.name = "Debug Label";
+        _debugText = go.GetComponent<TextMeshPro>();
     }
 
     // Update is called once per frame
@@ -35,14 +39,19 @@ public class MachineComponent : MonoBehaviour
     }
 
     void UpdateCondition() {
-        Condition -= BaseDeteriorationRate() * Time.deltaTime;
+        Condition -= Math.Max(BaseDeteriorationRate() * Time.deltaTime, 0);
+
+        _debugText.text = string.Format("Condition: {0:0.00}%", Condition * 100);
     }
 
     // measured in condition loss per second
     double BaseDeteriorationRate() {
+        const Double percentPerSecond = (1.0 / 60.0);
         switch (Type) {
+            case MachineComponentType.Battery:
+                return 2 * percentPerSecond;
             default:
-                return 1.0 / 60.0 * 100.0;
+                return 1 * percentPerSecond;
         }
     }
 }
