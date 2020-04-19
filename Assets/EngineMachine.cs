3ef045a6b376ed15b5edc3e4fb64f7ae28a87446
James Kelly <dxk2294@rit.edu>
2020-04-19 00:37:00 -0700

    Shut down FTL drive visuals when it's blocked

--- Assets/EngineMachine.cs
+++ Assets/EngineMachine.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EngineMachine : MachineController
 {
+    public Light reactionGlow;
+    public LinearTranslation engineFlapTranslationOne;
+    public LinearTranslation engineFlapTranslationTwo;
+
+
+    private StatusPoleLightColor _previousStatus;
+
     // Start is called before the first frame update
     void Start()
     {
         base.Start();
+
+        _previousStatus = statusPole.statusColor;
     }
 
     // Update is called once per frame
     void Update()
     {
         base.Update();
+
+        if(statusPole.statusColor != _previousStatus)
+        {
+            _previousStatus = statusPole.statusColor;
+
+            reactionGlow.enabled = statusPole.statusColor != StatusPoleLightColor.Error;
+            engineFlapTranslationOne.enabled = statusPole.statusColor != StatusPoleLightColor.Error;
+            engineFlapTranslationTwo.enabled = statusPole.statusColor != StatusPoleLightColor.Error;
+        }
     }
 }
