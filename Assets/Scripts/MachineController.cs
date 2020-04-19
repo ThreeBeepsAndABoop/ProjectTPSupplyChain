d5dbef05d49624d031c09a13ff69226de2da4caf
Jacob Ingalls <jacobingalls@yahoo.com>
2020-04-18 17:16:19 -0700

    Add controller and base machine framework.

---
+++ Assets/Scripts/MachineController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineController : MonoBehaviour
+{
+    public MachineTerminal terminal;
+    public StatusPole statusPole;
+
+    public double currentAmount, totalAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        terminal.Clear();
+        terminal.AppendLine("Example Status");
+
+        double progress = Math.Max(Math.Min(currentAmount / totalAmount, 1), 0);
+        terminal.AppendLine("Status: " + (Math.Round(progress * 100)).ToString() + "%");
+        terminal.AppendProgressLine(progress);
+    }
+}
---
+++ Assets/Scripts/MachineTerminal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class MachineTerminal : MonoBehaviour
+{
+
+    public TextMeshPro terminalText;
+
+    public Material sidePanelMaterial;
+    public List<GameObject> sidePanels;
+
+    public List<string> lines;
+
+    public int charsWide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject obj in sidePanels)
+        {
+            obj.GetComponent<Renderer>().material = sidePanelMaterial;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        terminalText.text = string.Join("\n", lines.ToArray());
+    }
+
+    public void Clear()
+    {
+        lines.Clear();
+    }
+
+    public void AppendLine(string line)
+    {
+        lines.Add(line);
+    }
+
+    public void AppendProgressLine(double progress)
+    {
+        int progressChars = (int)Math.Round((double)(charsWide - 2) * Math.Min(Math.Max(progress, 0), 1));
+        int nonProgressChars = (charsWide - 2) - progressChars;
+
+       this.AppendLine("[" + new string('=', progressChars) + new string(' ', nonProgressChars) + "]");
+    }
+}
